4a6a7841a4d02893eefde65442eb276865c4daea
Javier Li <mijahilbarra@gmail.com>
2025-12-12 01:10:56 -0500

    remove unsused folders

--- clase2/src/main/java/com/cibertec/clase2/repositories/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.clase2.repositories;
-
-import com.cibertec.clase2.models.Alumno;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlumnoRepository extends CrudRepository<Alumno, Integer> {
-}
